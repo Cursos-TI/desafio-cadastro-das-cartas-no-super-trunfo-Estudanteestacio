35c04daa085bdbb926b0e39de61433f07c360ccc
Estudanteestacio <guilhermealvesgodinho01@gmail.com>
2025-05-26 20:54:37 -0300

    Add files via upload

---
+++ carta super trunfo.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main(){
+    // Carta 01
+    char estado1[] = "ceara";
+    char codigo1[] = "01";
+    char cidade1[] = "fortaleza";
+    int populacao1 = 2680000;
+    float area1 = 314.94;
+    float pib1 = 67000.00;
+    int pontos1 = 10;
+
+    // Carta 02
+    char estado2[] = "sp";
+    char codigo2[] = "02";
+    char cidade2[] = "santos";
+    int populacao2 = 430000;
+    float area2 = 280.67;
+    float pib2 = 59000.00;
+    int pontos2 = 7;
+
+    printf("Carta 1\n");
+
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("Pib: %.2f\n", pib1);
+    printf("Pontos turísticos: %d\n", pontos1);
+
+    printf("\n");
+
+    printf("Carta 2\n");
+
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f\n", area2);
+    printf("Pib: %.2f\n", pib2);
+    printf("Pontos turísticos: %d\n", pontos2);
+
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
